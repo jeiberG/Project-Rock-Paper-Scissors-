7bbc41553e71347310b10df92590139871a0e4f2
jeiberG <jeibergarciayt@gmail.com>
2024-02-23 14:50:25 -0500

    Implementa la función playRound para jugar una sola ronda de Piedra, Papel o Tijera

--- game.js
+++ game.js
@@ -1,7 +1,28 @@
 function getComputerChoice() {
-    const choices = ['rock','paper','scissor'];
+    const choices = ['piedra','papel','tijera'];
     const ramdomIndex = Math.floor(Math.random() * choices.length);
     return choices[ramdomIndex] ;
 }
 
-console.log(getComputerChoice());
+
+function playRound(playerSelection, computerSelection) {
+
+    // Convertir ambas selecciones a minúsculas para comparación sin distinción entre mayúsculas y minúsculas
+    playerSelection = playerSelection.toLowerCase();
+    computerSelection = computerSelection.toLowerCase();
+
+    // Verificar todas las condiciones posibles en las que el jugador gana
+    const playerWins =
+    (playerSelection === "piedra" && computerSelection === "tijeras") ||
+    (playerSelection === "papel" && computerSelection === "piedra") ||
+    (playerSelection === "tijeras" && computerSelection === "papel");
+
+    if (playerWins) {
+        return "¡Ganaste! " + playerSelection + " vence a " + computerSelection;
+    } else if (playerSelection === computerSelection) {
+        return "Empate";
+    } else {
+        return "¡Perdiste! " + computerSelection + " vence a " + playerSelection;
+    }
+
+}
