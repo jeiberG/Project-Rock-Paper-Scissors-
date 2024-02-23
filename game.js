ca9613c0217abe7f6f6088063907d9b6980a72f3
jeiberG <jeibergarciayt@gmail.com>
2024-02-23 15:14:05 -0500

    Implementa la función playGame() para jugar cinco rondas de Piedra, Papel o Tijeras y determinar un ganador al final

--- game.js
+++ game.js
@@ -6,11 +6,6 @@ function getComputerChoice() {
 
 
 function playRound(playerSelection, computerSelection) {
-
-    // Convertir ambas selecciones a minúsculas para comparación sin distinción entre mayúsculas y minúsculas
-    playerSelection = playerSelection.toLowerCase();
-    computerSelection = computerSelection.toLowerCase();
-
     // Verificar todas las condiciones posibles en las que el jugador gana
     const playerWins =
     (playerSelection === "piedra" && computerSelection === "tijeras") ||
@@ -18,11 +13,45 @@ function playRound(playerSelection, computerSelection) {
     (playerSelection === "tijeras" && computerSelection === "papel");
 
     if (playerWins) {
-        return "¡Ganaste! " + playerSelection + " vence a " + computerSelection;
+        return "Ganaste";
     } else if (playerSelection === computerSelection) {
         return "Empate";
     } else {
-        return "¡Perdiste! " + computerSelection + " vence a " + playerSelection;
+        return "Perdiste";
     }
 
 }
+
+function playGame() {
+    let playerScore = 0;
+    let computerScore = 0;
+
+    for (let i = 0; i < 5; i++) {
+        const playerSelection = prompt("Elige: Piedra, Papel o Tijeras").toLowerCase();
+        const computerSelection = getComputerChoice();
+        const roundResult = playRound(playerSelection, computerSelection);
+
+        console.log("Ronda " + (i + 1) + ": " + roundResult);
+
+        if (roundResult.includes("Ganaste")) {
+            playerScore++;
+        } else if (roundResult.includes("Perdiste")) {
+            computerScore++;
+        }
+    }
+
+    console.log("Puntuación final:");
+    console.log("Jugador: " + playerScore);
+    console.log("Computadora: " + computerScore);
+
+    if (playerScore > computerScore) {
+        console.log("¡Felicidades! ¡Ganaste el juego!");
+    } else if (playerScore < computerScore) {
+        console.log("¡Lo siento! La computadora ganó el juego.");
+    } else {
+        console.log("¡El juego terminó en empate!");
+    }
+}
+
+
+playGame();
